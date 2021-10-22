9f91caaab27ddd73f2ec97c74082a5a48d2aa4d4
Linette <linette-kampmann@hotmail.dk>
2021-10-22 12:57:41 +0200

    Init

---
+++ Okonomen/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Okonomen.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
